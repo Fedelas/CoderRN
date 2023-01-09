ad43478dcf42a91552f38b4548a1cb6a935eb0e9
Federico Lasserre <federicolasserre@gmail.com>
2023-01-09 08:14:52 -0300

    change text Title and style

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.blueTitle}>Hola, Coder!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  blueTitle:{
+    color:'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
 });
